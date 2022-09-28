f25d62b835f68a839f91857e314aad07ec06525e
akopartem <Artemakopyan1401>
2022-09-28 12:06:44 +0300

    Initial commit

---
+++ src/main/java/ru/isntrui/imarui/ImaruiApplication.java
@@ -0,0 +1,13 @@
+package ru.isntrui.imarui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaruiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImaruiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/isntrui/imarui/ImaruiApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.isntrui.imarui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImaruiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+
+}
